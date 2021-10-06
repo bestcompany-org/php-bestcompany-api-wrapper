b4ec2392a39b31bf5808f54dc6ea38f4caa7a086
campope <cam.pope@gmail.com>
2021-10-06 17:01:24 -0600

    add companies endpoints

---
+++ src/Resources/Companies.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Bestcompany\BestcompanyApi\Resources;
+
+use Bestcompany\BestcompanyApi\Resources\Resource;
+
+class Companies extends Resource
+{
+  /**
+   * Get all companies.
+   *
+   * @return Object
+   */
+  public function all(array $params = []): Object
+  {
+    $path = 'companies';
+
+    return $this->client->request(
+      'get',
+      $path,
+      [],
+      http_build_query($params)
+    );
+  }
+
+  /**
+   * Get a Company.
+   *
+   * @param int $id
+   *
+   * @return Object
+   */
+  public function getById($id, $params = []): Object
+  {
+    $path = 'companies/' . $id;
+
+    return $this->client->request(
+      'get',
+      $path,
+      [],
+      http_build_query($params)
+    );
+  }
+
+  public function recommendations($params = []): Object
+  {
+    $path = 'companies/recommend';
+
+    return $this->client->request(
+      'post',
+      $path,
+      ['json' => $params],
+    );
+  }
+}
---
+++ tests/CompaniesTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Bestcompany\BestcompanyApi\Tests;
+
+use InvalidArgumentException;
+use Bestcompany\BestcompanyApi\BestcompanyApi;
+use Bestcompany\BestcompanyApi\Tests\BaseTestCase;
+
+class CompaniesTest extends BaseTestCase
+{
+    function test_it_requires_api_key(): void
+    {
+      $this->expectException(InvalidArgumentException::class);
+
+      $api = new BestcompanyApi([
+        'hostname' => $this->hostname
+      ]);
+
+      $response = $api->companies()->all();
+    }
+
+    function test_it_requires_valid_api_key(): void
+    {
+      $api = new BestcompanyApi([
+        'key' => 'asdfasdf',
+        'hostname' => $this->hostname
+      ]);
+
+      $response = $api->companies()->all();
+      $this->assertEquals(401, $response->getResponse()->getStatusCode());
+    }
+
+    function test_all_companies(): void
+    {
+      $api = new BestcompanyApi([
+        'key' => $this->key,
+        'hostname' => $this->hostname
+      ]);
+
+      $data = $api->companies()->all();
+      $this->assertObjectHasAttribute('current_page', $data);
+    }
+
+    function test_single_company(): void
+    {
+      $api = new BestcompanyApi([
+        'key' => $this->key,
+        'hostname' => $this->hostname
+      ]);
+
+      $data = $api->companies()->getById(1);
+      $this->assertObjectHasAttribute('id', $data);
+    }
+
+    function test_recommendation_of_companies(): void
+    {
+        $api = new BestcompanyApi([
+          'key' => $this->key,
+          'hostname' => $this->hostname
+        ]);
+
+        $data = $api->companies()->recommendations([
+          'mstep_only' => true,
+          'partners_only' => true
+        ]);
+        $this->assertObjectHasAttribute('recommended_companies', $data);
+    }
+}
