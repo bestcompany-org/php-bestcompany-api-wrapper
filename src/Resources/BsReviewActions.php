43c47d4c5e97b2d7dbac3e20e8008cd9a82e9a99
Cam Pope <cam.pope@gmail.com>
2022-01-21 16:07:55 -0700

    Add bsreviewactions

---
+++ src/Resources/BsReviewActions.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bestcompany\BestcompanyApi\Resources;
+
+use Bestcompany\BestcompanyApi\Resources\Resource;
+
+class BsReviewAction extends Resource
+{
+    /**
+     * Create a access request.
+     *
+     * @param array $params array of access request properties
+     *   *
+     * @return Object
+     */
+    public function create(array $params = []): Object
+    {
+      $path = 'bs-review-actions';
+
+      return $this->client->request(
+        'post',
+        $path,
+        ['json' => $params],
+      );
+    }
+}
