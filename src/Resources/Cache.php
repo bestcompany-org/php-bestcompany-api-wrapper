dadea2e7f9baf5c0a632f72000b334a604b33927
Cam Pope <cam.pope@gmail.com>
2022-03-11 12:13:35 -0700

    Add a cache resource

---
+++ src/Resources/Cache.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bestcompany\BestcompanyApi\Resources;
+
+use Bestcompany\BestcompanyApi\Resources\Resource;
+
+class Cache extends Resource
+{
+  /**
+   * Flush cache object by tag.
+   *
+   * @param array $tags
+   *   *
+   * @return Object
+   */
+  public function flushByTag(array $tags = []): Object
+  {
+    $path = 'bs-user-notifications';
+
+    return $this->client->request(
+      'post',
+      $path,
+      ['json' => [
+          'tags' => $tags
+        ]
+      ],
+    );
+  }
+}
